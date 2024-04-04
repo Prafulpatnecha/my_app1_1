6567f5a6256271a1848a99b78a438675e31bc811
Praful Patnecha <prafulpatnecha@gmail.com>
2024-04-04 14:02:40 +0530

    4-4-24

--- lib/home/component/contanardetails.dart
+++ lib/home/component/contanardetails.dart
@@ -43,3 +43,54 @@ Widget contanerdetails7()
     ),
   );
 }
+
+Widget contanerdetailsd8()
+{
+  return Container(
+    height: 330,
+    width: 330,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      shape: BoxShape.circle,
+      border: Border.all(
+        color: orange,
+        width: 38,
+      ),
+    ),
+    alignment:
+    Alignment.lerp(Alignment.bottomCenter, Alignment.topCenter, 4),
+    child: Container(
+      height: 248,
+      width: 248,
+      decoration: BoxDecoration(
+        color: orange,
+        shape: BoxShape.circle,
+      ),
+      alignment: Alignment.lerp(
+          Alignment.topRight,
+          Alignment.lerp(Alignment.topLeft, Alignment.bottomCenter, 1),
+          0.4),
+      child: Container(
+        height: 80,
+        width: 80,
+        alignment: Alignment.lerp(Alignment.topCenter, Alignment.topLeft, 2000),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+        ),
+        child: Container(
+          height: 79.9,
+          width: 79.9,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            // border: Border.all(
+            //   color: Colors.black,
+            //   width: 1,
+            // ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -92,53 +92,7 @@ void main() {
         appBar: appbardetails8(),
         bottomNavigationBar: bottomappbardetails(),
         body: Align(
-          child: Container(
-            height: 330,
-            width: 330,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: orange,
-                width: 38,
-              ),
-            ),
-            alignment:
-                Alignment.lerp(Alignment.bottomCenter, Alignment.topCenter, 4),
-            child: Container(
-              height: 248,
-              width: 248,
-              decoration: BoxDecoration(
-                color: orange,
-                shape: BoxShape.circle,
-              ),
-              alignment: Alignment.lerp(
-                  Alignment.topRight,
-                  Alignment.lerp(Alignment.topLeft, Alignment.bottomCenter, 1),
-                  0.4),
-              child: Container(
-                height: 80,
-                width: 80,
-                alignment: Alignment.lerp(Alignment.topCenter, Alignment.topLeft, 2000),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                ),
-                child: Container(
-                  height: 79.9,
-                  width: 79.9,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                    // border: Border.all(
-                    //   color: Colors.black,
-                    //   width: 1,
-                    // ),
-                  ),
-                ),
-              ),
-            ),
-          ),
+          child: contanerdetailsd8(),
         ),
       ),
     ),
